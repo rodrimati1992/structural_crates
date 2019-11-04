713fbd3768ec9e66a388a280707efe207b446c00
rodrimati1992 <rodrimatt1985@gmail.com>
2019-11-04 04:14:26 -0300

    Fixed root module doctest without alloc feature,identifier_macros_equality test.

--- structural/src/tests/macro_tests.rs
+++ structural/src/tests/macro_tests.rs
@@ -3,7 +3,7 @@
 #[cfg(feature="better_ti")]
 #[test]
 fn identifier_macros_equality(){
-    use crate::chars;
+    use crate::{chars,type_level::TString};
 
     fn assert_ident<T,U>(_ident:T)
     where
@@ -11,22 +11,22 @@ fn identifier_macros_equality(){
     {}
 
     {
-        type TheStr=chars::TString<(chars::_a,chars::_b,chars::_c,chars::_d)>;
+        type TheStr=TString<(chars::_a,chars::_b,chars::_c,chars::_d)>;
         assert_ident::<TheStr,TI!("abcd")>(ti!(abcd));
         assert_ident::<TheStr,TI!(a b c d)>(ti!(abcd));
         assert_ident::<TheStr,TI!(abcd)>(ti!(abcd));
     }
     {
-        type TheStr=chars::TString<(chars::_0,)>;
+        type TheStr=TString<(chars::_0,)>;
         assert_ident::<TheStr,TI!(0)>(ti!(0));
     }
     {
-        type TheStr=chars::TString<(chars::_2,chars::_1)>;
+        type TheStr=TString<(chars::_2,chars::_1)>;
         assert_ident::<TheStr,TI!(21)>(ti!(21));
         assert_ident::<TheStr,TI!(2 1)>(ti!(21));
     }
     {
-        type TheStr=chars::TString<(chars::_a,chars::_b,chars::_0)>;
+        type TheStr=TString<(chars::_a,chars::_b,chars::_0)>;
         assert_ident::<TheStr ,TI!(ab0)>(ti!(ab0));
     }
     
