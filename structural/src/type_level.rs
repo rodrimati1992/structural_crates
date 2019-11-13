934daffc3660f55e07cdab2a649d61668c0e320c
rodrimati1992 <rodrimatt1985@gmail.com>
2019-11-13 19:54:29 -0300

    Started rewrite to add nested field accessors.
    Commented out all the code outside of structural::type_level.
    
    Added structural::type_level::collection_traits module,
    with the {Append_,PushBack_,ToTList_,ToTuple_} traits,
    each one with a type alias for convenience.
    
    Implemented those traits for both tuples and TList/TNil.
    
    Removed TStringSet.
    Added FieldPath to represent a chain of fields (eg:`.a.b.c`).
    Added FieldPaths to represent a collection of FieldPath,
    with a type parameter determining whether there may be repeated FieldPath elements.

--- structural/src/type_level.rs
+++ structural/src/type_level.rs
@@ -4,10 +4,11 @@ Contains types representing values.
 
 // pub use core_extensions::type_level_bool::{self,True,False,Boolean};
 
+pub mod collection_traits;
 pub mod ident;
 pub mod list;
 
 pub use self::{
-    ident::{TStringSet,TString},
+    ident::{FieldPath,FieldPaths,MutableAccess,SharedAccess,TString},
     list::{TList,TNil},
 };
--- structural/src/type_level/ident.rs
+++ structural/src/type_level/ident.rs
@@ -8,10 +8,14 @@ use core_extensions::MarkerType;
 
 use std_::marker::PhantomData;
 
-
-
 pub use crate::chars::*;
+pub use crate::type_level::collection_traits::{
+    Append,Append_,
+    PushBack,PushBack_,
+};
 
+
+////////////////////////////////////////////////////////////////////////////////
 
 /// A type-level string,represented as a tuple of type-level bytes.
 ///
@@ -25,6 +29,7 @@ impl<T> TString<T>{
 
 impl<T> Copy for TString<T>{}
 impl<T> Clone for TString<T>{
+    #[inline(always)]
     fn clone(&self)->Self{
         *self
     }
@@ -32,34 +37,181 @@ impl<T> Clone for TString<T>{
 unsafe impl<T> MarkerType for TString<T>{}
 
 
-/// A type-level set of strings (`TString`s).
+////////////////////////////////////////////////////////////////////////////////
+
+
+/// A type-level representation of a chain of field accesses,like `.a.b.c.d`.
 ///
-pub struct TStringSet<T>(PhantomData<T>);
+pub struct FieldPath<T>(PhantomData<T>);
 
-impl<T> TStringSet<T>{
-    /// Constructs a `TStringSet`.
-    ///
-    /// # Safety
-    ///
-    /// `T` must be a tuple of `TString<_>`s,
-    /// where no `TString<_>` type is repeated within the tuple.
-    pub const unsafe fn new()->Self{
-        TStringSet(PhantomData)
-    }
-}
-
-impl<T> Copy for TStringSet<T>{}
-impl<T> Clone for TStringSet<T>{
+impl<T> Copy for FieldPath<T>{}
+impl<T> Clone for FieldPath<T>{
     fn clone(&self)->Self{
         *self
     }
 }
 
-// `MarkerType` is not implemented for `TStringSet` 
-// because `TStringSet` ought only be constructible
-// by satisfying the safety requirements of `TStringSet::new`,
+impl<T> FieldPath<T>{
+    pub const NEW:Self=FieldPath(PhantomData);
+
+    pub const fn new()->FieldPath<T>{
+        FieldPath(PhantomData)
+    }
+}
+
+unsafe impl<T> MarkerType for FieldPath<T>{}
+
+
+impl<T,S> PushBack_<TString<S>> for FieldPath<T>
+where
+    T:PushBack_<TString<S>>
+{
+    type Output=FieldPath<PushBack<T,TString<S>>>;
+}
+
+impl<T,S> PushBack_<FieldPath<(S,)>> for FieldPath<T>
+where
+    T:PushBack_<S>
+{
+    type Output=FieldPath<PushBack<T,S>>;
+}
+
+
+impl<T,U> Append_<FieldPath<U>> for FieldPath<T>
+where
+    T:Append_<U>
+{
+    type Output=FieldPath<Append<T,U>>;
+}
+
+
+impl<T> FieldPath<T>{
+    #[inline(always)]
+    pub fn push<U,O>(self,_other:U)->FieldPath<O>
+    where
+        Self:PushBack_<U,Output=FieldPath<O>>
+    {
+        MarkerType::MTVAL
+    }
+
+    #[inline(always)]
+    pub fn append<U>(self,_other:FieldPath<U>)->FieldPath<Append<T,U>>
+    where
+        T:Append_<U>
+    {
+        MarkerType::MTVAL
+    }
+}
+
+
+////////////////////////////////////////////////////////////////////////////////
+
+pub struct FieldPaths<T,A>(PhantomData<(T,A)>);
+
+pub struct MutableAccess;
+pub struct SharedAccess;
+
+
+impl<T> FieldPaths<T,MutableAccess>{
+    /// Constructs a `FieldPaths`.
+    ///
+    /// # Safety
+    ///
+    /// `T` must be a tuple of `TString<_>`s,
+    /// where no `TString<_>` type is repeated within the tuple.
+    #[inline(always)]
+    pub const unsafe fn new()->Self{
+        FieldPaths(PhantomData)
+    }
+}
+
+impl<T> FieldPaths<T,SharedAccess>{
+    /// Constructs a `FieldPaths`.
+    #[inline(always)]
+    pub const fn new()->Self{
+        FieldPaths(PhantomData)
+    }
+}
+
+impl<T,A> From<FieldPaths<(FieldPath<T>,),A>> for FieldPath<T>{
+    #[inline(always)]
+    fn from(_this:FieldPaths<(FieldPath<T>,),A>)->Self{
+        MarkerType::MTVAL
+    }
+}
+
+impl<T,A> FieldPaths<(FieldPath<T>,),A> {
+    #[inline(always)]
+    pub fn to_path(self)->FieldPath<T>{
+        MarkerType::MTVAL
+    }
+}
+
+
+impl<T,A> FieldPaths<T,A>{
+    #[inline(always)]
+    pub fn push<U,O>(self,_other:U)->FieldPaths<O,SharedAccess>
+    where
+        Self:PushBack_<U,Output=FieldPaths<O,SharedAccess>>
+    {
+        MarkerType::MTVAL
+    }
+
+    #[inline(always)]
+    pub fn append<U,A2>(self,_other:FieldPaths<U,A2>)->FieldPaths<Append<T,U>,SharedAccess>
+    where
+        T:Append_<U>
+    {
+        MarkerType::MTVAL
+    }
+}
+
+
+impl<T,Access> Copy for FieldPaths<T,Access>{}
+
+impl<T,Access> Clone for FieldPaths<T,Access>{
+    #[inline(always)]
+    fn clone(&self)->Self{
+        *self
+    }
+}
+
+// `MarkerType` is not implemented for `FieldPaths<T.MutableAccess>` 
+// because `FieldPaths<T.MutableAccess>` ought only be constructible
+// by satisfying the safety requirements of `FieldPaths::<T.MutableAccess>::new`,
 // which aren't cheaply enforceable on the type level.
 //
-// impl<T> !MarkerType for TStringSet<T>{}
+// impl<T> !MarkerType for FieldPaths<T.MutableAccess>{}
+
+unsafe impl<T> MarkerType for FieldPaths<T,SharedAccess>{}
 
 
+impl<T,A,P> PushBack_<FieldPath<P>> for FieldPaths<T,A>
+where
+    T:PushBack_<FieldPath<P>>
+{
+    type Output=FieldPaths<PushBack<T,FieldPath<P>>,SharedAccess>;
+}
+
+
+impl<T,A,P,A2> PushBack_<FieldPaths<(P,),A2>> for FieldPaths<T,A>
+where
+    T:PushBack_<P>
+{
+    type Output=FieldPaths<PushBack<T,P>,SharedAccess>;
+}
+
+impl<T,T2,A,A2> Append_<FieldPaths<T2,A2>> for FieldPaths<T,A>
+where
+    T:Append_<T2>
+{
+    type Output=FieldPaths<Append<T,T2>,SharedAccess>;
+}
+
+
+
+////////////////////////////////////////////////////////////////////////////////
+
+
+#[cfg(test)]
+mod tests;
