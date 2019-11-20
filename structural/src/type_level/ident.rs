471b7bf601e9ed06967ecd855a10bd2bec932b9e
rodrimati1992 <rodrimatt1985@gmail.com>
2019-11-20 19:49:32 -0300

    Changes to `structural::type_level` module.
    Renamed FieldPaths to FieldPathSet,renamed its `A` type parameter to `U`,
    renamed `MutableAccess`to `UniquePaths`,
    renamed `SharedAccess`to `AliasedPaths`..
    
    Added ToTString_,implemented it for `TString<_>` and `FieldPath<(TString<_>,)>`
    Added Flatten_ trait,implemented it for tuples and TList/TNil,
    
    Added IsFieldPath marker trait for FieldPath
    Added IsFieldPathSet marker trait for FieldPathSet
    
    Added TString::{to_path,to_set} methods.
    Added FieldPath::{to_path,to_set,to_tstr} methods.
    Added FieldPathSet::{downgrade} methods.
    
    Added From impls among TString,FieldPath,and FieldPathSet.
    
    Implemented ToTlist_ for `FieldPath<_>`.
    
    Updated tests in `structural::type_level::ident::tests`

--- structural/src/type_level/ident.rs
+++ structural/src/type_level/ident.rs
@@ -12,19 +12,61 @@ pub use crate::chars::*;
 pub use crate::type_level::collection_traits::{
     Append,Append_,
     PushBack,PushBack_,
+    ToTList,ToTList_,
+    ToTString_,
 };
+
+
+////////////////////////////////////////////////////////////////////////////////
+
+mod sealed{
+    pub trait Sealed{}
+}
+use self::sealed::Sealed;
+
+impl<T> Sealed for TString<T>{}
+impl<T> Sealed for FieldPath<T>{}
+impl<T,U> Sealed for FieldPathSet<T,U>{}
+
+
+/// A marker trait only implemented by FieldPath
+pub trait IsFieldPath:Sealed+Copy{}
+
+impl<T> IsFieldPath for FieldPath<T>{}
+
+
+/// A marker trait only implemented by FieldPathSet
+pub trait IsFieldPathSet:Sealed+Copy{
+    /// Whether the pats in the set can contain duplicate paths.
+    type PathUniqueness;
+}
+
+impl<T,U> IsFieldPathSet for FieldPathSet<T,U>{
+    type PathUniqueness=U;
+}
 
 
 ////////////////////////////////////////////////////////////////////////////////
 
 /// A type-level string,represented as a tuple of type-level bytes.
 ///
-/// This cannot be converted to a `&'static str` constant.
+/// This cannot be converted to a `&'static str` constant
+/// (if you can figure out a cheap way to do that please create an issue/pull request).
 pub struct TString<T>(PhantomData<T>);
 
 impl<T> TString<T>{
     /// Constructs the TString.
     pub const NEW:Self=TString(PhantomData);
+
+    #[inline(always)]
+    pub const fn to_path(self)->FieldPath<(TString<T>,)>{
+        FieldPath::NEW
+    }
+
+    #[inline(always)]
+    pub const fn to_set(self)->FieldPathSet<(FieldPath<(TString<T>,)>,),UniquePaths>{
+        FieldPath::NEW.to_set()
+    }
 }
 
 impl<T> Copy for TString<T>{}
@@ -37,15 +79,31 @@ impl<T> Clone for TString<T>{
 unsafe impl<T> MarkerType for TString<T>{}
 
 
+impl<T> ToTString_ for TString<T>{
+    type Output=Self;
+}
+
+
+
+
 ////////////////////////////////////////////////////////////////////////////////
 
 
-/// A type-level representation of a chain of field accesses,like `.a.b.c.d`.
+/// U type-level representation of a chain of field accesses,like `.a.b.c.d`.
 ///
 pub struct FieldPath<T>(PhantomData<T>);
 
+/// A FieldPath for accesing a single `Str` field.
+pub type FieldPath1<Str>=FieldPath<(Str,)>;
+
+
+/// A FieldPath constructed in the same way that TString is.
+#[doc(hidden)]
+pub type FieldPathString<Str>=FieldPath<(TString<Str>,)>;
+
 impl<T> Copy for FieldPath<T>{}
 impl<T> Clone for FieldPath<T>{
+    #[inline(always)]
     fn clone(&self)->Self{
         *self
     }
@@ -54,6 +112,7 @@ impl<T> Clone for FieldPath<T>{
 impl<T> FieldPath<T>{
     pub const NEW:Self=FieldPath(PhantomData);
 
+    #[inline(always)]
     pub const fn new()->FieldPath<T>{
         FieldPath(PhantomData)
     }
@@ -61,6 +120,16 @@ impl<T> FieldPath<T>{
 
 unsafe impl<T> MarkerType for FieldPath<T>{}
 
+impl<S> ToTString_ for FieldPath<(TString<S>,)>{
+    type Output=TString<S>;
+}
+
+impl<T> ToTList_ for FieldPath<T>
+where
+    T:ToTList_
+{
+    type Output=ToTList<T>;
+}
 
 impl<T,S> PushBack_<TString<S>> for FieldPath<T>
 where
@@ -87,9 +156,9 @@ where
 
 impl<T> FieldPath<T>{
     #[inline(always)]
-    pub fn push<U,O>(self,_other:U)->FieldPath<O>
+    pub fn push<U,V>(self,_other:U)->FieldPath<V>
     where
-        Self:PushBack_<U,Output=FieldPath<O>>
+        Self:PushBack_<U,Output=FieldPath<V>>
     {
         MarkerType::MTVAL
     }
@@ -101,19 +170,77 @@ impl<T> FieldPath<T>{
     {
         MarkerType::MTVAL
     }
+
+    #[inline(always)]
+    pub const fn to_set(self)->FieldPathSet<(Self,),UniquePaths>{
+        unsafe{
+            FieldPathSet::new_unchecked()
+        }
+    }
+}
+
+impl<S> FieldPath<(TString<S>,)>{
+    pub const fn to_tstr(self)->TString<S>{
+        MarkerType::MTVAL
+    }
 }
 
 
 ////////////////////////////////////////////////////////////////////////////////
 
-pub struct FieldPaths<T,A>(PhantomData<(T,A)>);
+/// A list of `FieldPath`s whose uniqueness is determined by `U`.
+///
+/// If `U=UniquePaths` then all the `FieldPath`s are unique,
+/// and this can be passed to `GetFieldExt::fields_mut`,
+/// since you can't have aliasing mutable references to the same field.
+///
+/// If `U=AliasedPaths` then there might be repeated `FieldPath`s,
+/// and this cannot be passed to `GetFieldExt::fields_mut`,
+/// because it might borrow the same field mutably twice.
+///
+pub struct FieldPathSet<T,U>(PhantomData<(T,U)>);
 
-pub struct MutableAccess;
-pub struct SharedAccess;
+
+/// A merker type indicating that FieldPathSet contains unique paths,
+/// in which no path is a prefix of any other path in the set,
+/// this is required to call `GetFieldExt::fields_mut`.
+pub struct UniquePaths;
+
+/// A merker type indicating that FieldPathSet may not contain unique `FielsPath`s,
+/// which means that its not safe to pass the FieldPathSet to `GetFieldExt::fields_mut`
+/// (this is why it requires `FieldPathSet<_,UniquePaths>`).
+pub struct AliasedPaths;
 
 
-impl<T> FieldPaths<T,MutableAccess>{
-    /// Constructs a `FieldPaths`.
+impl<T,U> Copy for FieldPathSet<T,U>{}
+
+impl<T,U> Clone for FieldPathSet<T,U>{
+    #[inline(always)]
+    fn clone(&self)->Self{
+        *self
+    }
+}
+
+// `MarkerType` is not implemented for `FieldPathSet<T.UniquePaths>` 
+// because `FieldPathSet<T.UniquePaths>` ought only be constructible
+// by satisfying the safety requirements of `FieldPathSet::<T.UniquePaths>::new`,
+// which aren't cheaply enforceable on the type level.
+//
+// impl<T> !MarkerType for FieldPathSet<T.UniquePaths>{}
+
+unsafe impl<T> MarkerType for FieldPathSet<T,AliasedPaths>{}
+
+impl<T,U> FieldPathSet<T,U>{
+    // The constructor function used by proc macros,
+    // this avoids having to use .
+    #[doc(hidden)]
+    #[inline(always)]
+    pub const unsafe fn new_unchecked()->Self{
+        FieldPathSet(PhantomData)
+    }
+}
+impl<T> FieldPathSet<T,UniquePaths>{
+    /// Constructs a `FieldPathSet`.
     ///
     /// # Safety
     ///
@@ -121,91 +248,125 @@ impl<T> FieldPaths<T,MutableAccess>{
     /// where no `TString<_>` type is repeated within the tuple.
     #[inline(always)]
     pub const unsafe fn new()->Self{
-        FieldPaths(PhantomData)
+        FieldPathSet(PhantomData)
+    }
+
+    /// Converts a `FieldPathSet<T,UniquePaths>` to a `FieldPathSet<T,AliasedPaths>`
+    #[inline(always)]
+    pub const fn downgrade(self)->FieldPathSet<T,AliasedPaths>{
+        FieldPathSet(PhantomData)
     }
 }
 
-impl<T> FieldPaths<T,SharedAccess>{
-    /// Constructs a `FieldPaths`.
+impl<T> FieldPathSet<T,AliasedPaths>{
+    /// Constructs a `FieldPathSet`.
     #[inline(always)]
     pub const fn new()->Self{
-        FieldPaths(PhantomData)
+        FieldPathSet(PhantomData)
+    }
+
+
+    /// Converts a `FieldPathSet<T,AliasedPaths>` to a `FieldPathSet<T,UniquePaths>`
+    ///
+    /// # Safety
+    ///
+    /// You must ensure that all the `FieldPath`s are unique,
+    /// there must be no `FieldPath` that is a prefix of any other `FieldPath`.
+    #[inline(always)]
+    pub const unsafe fn upgrade_unchecked(self)->FieldPathSet<T,AliasedPaths>{
+        FieldPathSet(PhantomData)
     }
 }
 
-impl<T,A> From<FieldPaths<(FieldPath<T>,),A>> for FieldPath<T>{
+impl<T,U> FieldPathSet<(FieldPath<T>,),U> {
     #[inline(always)]
-    fn from(_this:FieldPaths<(FieldPath<T>,),A>)->Self{
+    pub const fn to_path(self)->FieldPath<T>{
         MarkerType::MTVAL
     }
 }
 
-impl<T,A> FieldPaths<(FieldPath<T>,),A> {
-    #[inline(always)]
-    pub fn to_path(self)->FieldPath<T>{
-        MarkerType::MTVAL
-    }
-}
 
-
-impl<T,A> FieldPaths<T,A>{
+impl<T,U> FieldPathSet<T,U>{
     #[inline(always)]
-    pub fn push<U,O>(self,_other:U)->FieldPaths<O,SharedAccess>
+    pub fn push<O,Out>(self,_other:O)->FieldPathSet<Out,AliasedPaths>
     where
-        Self:PushBack_<U,Output=FieldPaths<O,SharedAccess>>
+        Self:PushBack_<O,Output=FieldPathSet<Out,AliasedPaths>>
     {
         MarkerType::MTVAL
     }
 
     #[inline(always)]
-    pub fn append<U,A2>(self,_other:FieldPaths<U,A2>)->FieldPaths<Append<T,U>,SharedAccess>
+    pub fn append<T2,U2>(
+        self,
+        _other:FieldPathSet<T2,U2>
+    )->FieldPathSet<Append<T,T2>,AliasedPaths>
     where
-        T:Append_<U>
+        T:Append_<T2>
     {
         MarkerType::MTVAL
     }
 }
 
 
-impl<T,Access> Copy for FieldPaths<T,Access>{}
-
-impl<T,Access> Clone for FieldPaths<T,Access>{
-    #[inline(always)]
-    fn clone(&self)->Self{
-        *self
-    }
+impl<T,U> ToTList_ for FieldPathSet<T,U>
+where
+    T:ToTList_
+{
+    type Output=ToTList<T>;
 }
 
-// `MarkerType` is not implemented for `FieldPaths<T.MutableAccess>` 
-// because `FieldPaths<T.MutableAccess>` ought only be constructible
-// by satisfying the safety requirements of `FieldPaths::<T.MutableAccess>::new`,
-// which aren't cheaply enforceable on the type level.
-//
-// impl<T> !MarkerType for FieldPaths<T.MutableAccess>{}
-
-unsafe impl<T> MarkerType for FieldPaths<T,SharedAccess>{}
-
-
-impl<T,A,P> PushBack_<FieldPath<P>> for FieldPaths<T,A>
+impl<T,U,P> PushBack_<FieldPath<P>> for FieldPathSet<T,U>
 where
     T:PushBack_<FieldPath<P>>
 {
-    type Output=FieldPaths<PushBack<T,FieldPath<P>>,SharedAccess>;
+    type Output=FieldPathSet<PushBack<T,FieldPath<P>>,AliasedPaths>;
 }
 
 
-impl<T,A,P,A2> PushBack_<FieldPaths<(P,),A2>> for FieldPaths<T,A>
+impl<T,U,P,U2> PushBack_<FieldPathSet<(P,),U2>> for FieldPathSet<T,U>
 where
     T:PushBack_<P>
 {
-    type Output=FieldPaths<PushBack<T,P>,SharedAccess>;
+    type Output=FieldPathSet<PushBack<T,P>,AliasedPaths>;
 }
 
-impl<T,T2,A,A2> Append_<FieldPaths<T2,A2>> for FieldPaths<T,A>
+impl<T,T2,U,U2> Append_<FieldPathSet<T2,U2>> for FieldPathSet<T,U>
 where
     T:Append_<T2>
 {
-    type Output=FieldPaths<Append<T,T2>,SharedAccess>;
+    type Output=FieldPathSet<Append<T,T2>,AliasedPaths>;
+}
+
+
+
+////////////////////////////////////////////////////////////////////////////////
+
+
+impl<S> From<FieldPath<(TString<S>,)>> for TString<S>{
+    #[inline(always)]
+    fn from(_this:FieldPath<(TString<S>,)>)->Self{
+        MarkerType::MTVAL
+    }
+}
+
+impl<S> From<TString<S>> for FieldPath<(TString<S>,)>{
+    #[inline(always)]
+    fn from(_this:TString<S>)->Self{
+        MarkerType::MTVAL
+    }
+}
+impl<T,U> From<FieldPathSet<(FieldPath<T>,),U>> for FieldPath<T>{
+    #[inline(always)]
+    fn from(_this:FieldPathSet<(FieldPath<T>,),U>)->Self{
+        MarkerType::MTVAL
+    }
+}
+
+impl<P> From<FieldPath<P>> for FieldPathSet<(FieldPath<P>,),UniquePaths>{
+    #[inline(always)]
+    fn from(this:FieldPath<P>)->Self{
+        this.to_set()
+    }
 }
 
 
