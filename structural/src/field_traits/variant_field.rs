a956210f16c7878969e5202553fe73b4380abbe2
rodrimati1992 <rodrimatt1985@gmail.com>
2020-01-10 05:20:45 -0300

    Changes to [Into]GetField[Mut],and *VariantField* traits.
    Moved Ty associated type from `GetField` into the new `FieldType` trait.
    
    Added a defaulted `Param=()` type parameter to the [Into]GetField[Mut] traits,
    for use in the implementation of the unchecked variant field access.
    
    Added `name:FieldName, param:Param` parameters to all the accessor methods,
    removing the `_:PhantomData<FieldName>` parameter in the raw pointer one.
    
    Replaced the trait aliases for the [Into]GetField[Mut] traits with invocations of the
    newly defined `declare_accessor_trait_alias` macro.
    
    Replaced the manual impls of accessor traits for Box/Rc/Arc types with the
    delegation macro.
    
    Changed the `*VariantField*` traits to be unsafe marker traits,
    which require implementing `IsVariant<FieldPath1<V>>` and
    `GetFieldImpl<VariantFieldPath<V, F>,UncheckedVariantField<V, F>>`
    
    Declared trait aliases for optional and non-optional versions of the `*VariantField*` traits,
    using the newly declared `declare_trait_alias` macro to declare then.

--- structural/src/field_traits/variant_field.rs
+++ structural/src/field_traits/variant_field.rs
@@ -8,49 +8,120 @@ unsafe to call because the enum must be the variant specified by
 the `V` generic parameter.
 */
 
-use crate::field_traits::FieldErr;
+use crate::{
+    enum_traits::IsVariant,
+    field_traits::{
+        IsFieldErr, NonOptField, OptGetField, OptGetFieldMut, OptIntoField, OptionalField,
+    },
+    type_level::{FieldPath1, UncheckedVariantField, VariantFieldPath},
+    GetFieldImpl, GetFieldMutImpl, IntoFieldImpl,
+};
 
 use std_::marker::PhantomData;
 
-/// Gets a shared reference to the `F` field  from the `V` variant
-pub trait GetVariantField<V, F> {
-    type Ty;
-    type Err: FieldErr;
+#[cfg(feature = "alloc")]
+pub use crate::alloc::boxed::Box;
 
-    /// Gets a shared reference to the `F` field  from the `V` variant
-    ///
-    /// # Safety
-    ///
-    /// The variant of this enum must be the one specified by the `V` generic parameter
-    unsafe fn get_variant_field(&self) -> Result<&Self::Ty, Self::Err>;
+macro_rules! declare_trait_alias {
+    (
+        $(#[$attr:meta])*
+        $vis:vis trait $trait_name:ident< $vari:ident, $field:ident >=
+        $($supertraits:tt)*
+    ) => (
+        $(#[$attr])*
+        $vis trait $trait_name< $vari, $field >:$($supertraits)* {}
+
+        impl<This,$vari,$field> $trait_name<$vari,$field> for This
+        where
+            This:?Sized+$($supertraits)*
+        {}
+    )
+}
+
+///////////////////////////
+
+/// Gets a shared reference to the `F` field  from the `V` variant
+///
+/// # Safety
+///
+/// TODO
+pub unsafe trait GetVariantFieldImpl<V, F>:
+    IsVariant<FieldPath1<V>> + GetFieldImpl<VariantFieldPath<V, F>, UncheckedVariantField<V, F>>
+{
 }
 
 /// Gets a mutable reference to the `F` field  from the `V` variant
-pub unsafe trait GetVariantFieldMut<V, F>: GetVariantField<V, F> {
-    /// Gets a mutable reference to the `F` field  from the `V` variant
-    ///
-    /// # Safety
-    ///
-    /// The variant of this enum must be the one specified by the `V` generic parameter
-    unsafe fn get_variant_field_mut(&mut self) -> Result<&mut Self::Ty, Self::Err>;
-
-    /// Gets a mutable pointer to the `F` field  from the `V` variant
-    ///
-    /// # Safety
-    ///
-    /// The variant of this enum must be the one specified by the `V` generic parameter
-    unsafe fn get_variant_field_raw_mut(
-        ptr: *mut Self,
-        _: PhantomData<(V, F)>,
-    ) -> Result<*mut Self::Ty, Self::Err>;
+///
+/// # Safety
+///
+/// This has the safety requirements as `GetFieldMutImpl`.
+///
+/// TODO: requirements for the VariantField traits.
+pub unsafe trait GetVariantFieldMutImpl<V, F>:
+    GetVariantFieldImpl<V, F> + GetFieldMutImpl<VariantFieldPath<V, F>, UncheckedVariantField<V, F>>
+{
 }
 
 /// Converts this into the `F` field  from the `V` variant
-pub trait IntoVariantField<V, F>: GetVariantField<V, F> {
-    /// Converts this into the `F` field  from the `V` variant
-    ///
-    /// # Safety
-    ///
-    /// The variant of this enum must be the one specified by the `V` generic parameter
-    unsafe fn into_variant_field(self) -> Result<Self::Ty, Self::Err>;
+///
+/// # Safety
+///
+/// This has the safety requirements as `GetFieldMutImpl`.
+///
+/// TODO: requirements for the VariantField traits.
+pub unsafe trait IntoVariantFieldImpl<V, F>:
+    GetVariantFieldImpl<V, F> + IntoFieldImpl<VariantFieldPath<V, F>, UncheckedVariantField<V, F>>
+{
+}
+
+///////////////////////////
+
+declare_trait_alias! {
+    pub trait GetVariantField<V,F>=
+        OptGetField<VariantFieldPath<V, F>> +
+        GetVariantFieldImpl<V, F, Err= NonOptField> +
+}
+
+declare_trait_alias! {
+    pub trait GetVariantFieldMut<V, F>=
+        OptGetFieldMut<VariantFieldPath<V, F>> +
+        GetVariantFieldMutImpl<V, F, Err= NonOptField> +
+}
+
+declare_trait_alias! {
+    pub trait IntoVariantField<V, F>=
+        OptIntoField<VariantFieldPath<V, F>> +
+        IntoVariantFieldImpl<V, F, Err= NonOptField> +
+}
+
+declare_trait_alias! {
+    pub trait IntoVariantFieldMut<V, F>=
+        GetVariantFieldMut<V, F>+
+        IntoVariantField<V, F>+
+}
+
+///////////////////////////
+
+declare_trait_alias! {
+    pub trait OptGetVariantField<V, F>=
+        OptGetField<VariantFieldPath<V, F>> +
+        GetVariantFieldImpl<V, F, Err= OptionalField> +
+}
+
+declare_trait_alias! {
+    pub trait OptGetVariantFieldMut<V, F>=
+        OptGetFieldMut<VariantFieldPath<V, F>> +
+        GetVariantFieldMutImpl<V, F, Err= OptionalField> +
+}
+
+declare_trait_alias! {
+    pub trait OptIntoVariantField<V, F>=
+        OptIntoField<VariantFieldPath<V, F>> +
+        IntoVariantFieldImpl<V, F, Err= OptionalField> +
+}
+
+declare_trait_alias! {
+    pub trait OptIntoVariantFieldMut<V, F>=
+        OptGetVariantFieldMut<V, F>+
+        OptIntoVariantField<V, F>+
 }
