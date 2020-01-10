e2129b9232c017fd4e9ecabab91c7c44a4df61ff
rodrimati1992 <rodrimatt1985@gmail.com>
2020-01-10 06:33:21 -0300

    Defined the EnumExt trait,made IsVariant unsafe,defined more field path types.
    The EnumExt trait provides extensio methods to wrap Self in VariantProxy
    if it is the variant whose name is passed as a parameter.
    
    Made the IsVariant trait unsafe,whose safety invariant is relied on by VariantProxy.
    
    Added the from_box constructor to construct a `Box<VariantProxy<T,V>>` from a `Box<T>`.
    
    Rewrote VariantProxy accessor impls due to the rewrite.
    
    Defined the VariantName marker type,to represent the name of a variant in a field path.
    
    Defined the UncheckedVariantField marker type,
    to access a variant field without checking that an enum is that variant.
    
    Implemented Copy+Clone and constructors for VariantField / VariantName / UncheckedVariantField.
    
    Removed the MakeUnit trait because it's unused.

---
+++ structural/src/enum_traits/enum_ext.rs
@@ -0,0 +1,73 @@
+#[cfg(feature = "alloc")]
+use crate::alloc::boxed::Box;
+
+use crate::{enum_traits::VariantProxy, type_level::FieldPath1};
+
+pub trait EnumExt {
+    fn as_variant<V>(&self, vari: FieldPath1<V>) -> Option<&VariantProxy<Self, FieldPath1<V>>>
+    where
+        Self: IsVariant<FieldPath1<V>>,
+    {
+        if IsVariant::is_variant_(self, vari) {
+            unsafe { Some(VariantProxy::from_ref(self)) }
+        } else {
+            None
+        }
+    }
+
+    fn as_mut_variant<V>(
+        &mut self,
+        vari: FieldPath1<V>,
+    ) -> Option<&mut VariantProxy<Self, FieldPath1<V>>>
+    where
+        Self: IsVariant<FieldPath1<V>>,
+    {
+        if IsVariant::is_variant_(&*self, vari) {
+            unsafe { Some(VariantProxy::from_mut(self)) }
+        } else {
+            None
+        }
+    }
+
+    unsafe fn as_raw_mut_variant<V>(
+        this: *mut Self,
+        vari: FieldPath1<V>,
+    ) -> Option<*mut VariantProxy<Self, FieldPath1<V>>>
+    where
+        Self: IsVariant<FieldPath1<V>> + Sized,
+    {
+        if IsVariant::is_variant_(&*this, vari) {
+            Some(VariantProxy::from_raw_mut(this))
+        } else {
+            None
+        }
+    }
+
+    fn into_variant<V>(self, vari: FieldPath1<V>) -> Option<VariantProxy<Self, FieldPath1<V>>>
+    where
+        Self: IsVariant<FieldPath1<V>> + Sized,
+    {
+        if IsVariant::is_variant_(&self, vari) {
+            unsafe { Some(VariantProxy::new(self)) }
+        } else {
+            None
+        }
+    }
+
+    #[cfg(feature = "alloc")]
+    fn box_into_variant<V>(
+        self: Box<Self>,
+        vari: FieldPath1<V>,
+    ) -> Option<Box<VariantProxy<Self, FieldPath1<V>>>>
+    where
+        Self: IsVariant<FieldPath1<V>> + Sized,
+    {
+        if IsVariant::is_variant_(&*self, vari) {
+            unsafe { Some(VariantProxy::from_box(self)) }
+        } else {
+            None
+        }
+    }
+}
+
+impl<This: ?Sized> EnumExt for This {}
--- structural/src/utils.rs
+++ structural/src/utils.rs
@@ -29,30 +29,6 @@ impl<T> OptionParam_ for Option<T> {
 
 /////////////////////////////////////////////////////////
 
-#[doc(hidden)]
-pub trait MakeUnit {
-    const UNIT: Self;
-}
-
-impl<'a> MakeUnit for () {
-    const UNIT: Self = ();
-}
-
-impl<'a> MakeUnit for &'a () {
-    const UNIT: Self = &();
-}
-
-impl<'a> MakeUnit for *mut () {
-    const UNIT: Self = 1024 as *mut ();
-}
-
-#[inline(always)]
-pub fn unit_mut_ref<'a>() -> &'a mut () {
-    unsafe { &mut *(1024 as *mut ()) }
-}
-
-/////////////////////////////////////////////////////////
-
 #[inline(always)]
 pub unsafe fn option_as_mut_result<T>(ptr: *mut Option<T>) -> Result<*mut T, OptionalField> {
     match *ptr {
