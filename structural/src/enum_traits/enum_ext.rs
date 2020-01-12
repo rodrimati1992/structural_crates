8dec166a809b873a432b58fe20133c0dbb6ce1bd
rodrimati1992 <rodrimatt1985@gmail.com>
2020-01-12 03:33:17 -0300

    Changed EnumExt methods to return a Result.
    The EnumExt methods now return `Err(self)` if the variant doesn't match.
    
    Rewrote the `Rev*` trait impls for `VariantName` to use EnumExt.

--- structural/src/enum_traits/enum_ext.rs
+++ structural/src/enum_traits/enum_ext.rs
@@ -7,68 +7,79 @@ use crate::{
 };
 
 pub trait EnumExt {
-    fn as_variant<V>(&self, vari: FieldPath1<V>) -> Option<&VariantProxy<Self, FieldPath1<V>>>
+    #[inline(always)]
+    fn as_variant<V>(
+        &self,
+        vari: FieldPath1<V>
+    ) -> Result<&VariantProxy<Self, FieldPath1<V>>,&Self>
     where
         Self: IsVariant<FieldPath1<V>>,
     {
         if IsVariant::is_variant_(self, vari) {
-            unsafe { Some(VariantProxy::from_ref(self)) }
+            unsafe { Ok(VariantProxy::from_ref(self)) }
         } else {
-            None
+            Err(self)
         }
     }
 
+    #[inline(always)]
     fn as_mut_variant<V>(
         &mut self,
         vari: FieldPath1<V>,
-    ) -> Option<&mut VariantProxy<Self, FieldPath1<V>>>
+    ) -> Result<&mut VariantProxy<Self, FieldPath1<V>>,&mut Self>
     where
         Self: IsVariant<FieldPath1<V>>,
     {
         if IsVariant::is_variant_(&*self, vari) {
-            unsafe { Some(VariantProxy::from_mut(self)) }
+            unsafe { Ok(VariantProxy::from_mut(self)) }
         } else {
-            None
+            Err(self)
         }
     }
 
+    #[inline(always)]
     unsafe fn as_raw_mut_variant<V>(
         this: *mut Self,
         vari: FieldPath1<V>,
-    ) -> Option<*mut VariantProxy<Self, FieldPath1<V>>>
+    ) -> Result<*mut VariantProxy<Self, FieldPath1<V>>,*mut Self>
     where
-        Self: IsVariant<FieldPath1<V>> + Sized,
+        Self: IsVariant<FieldPath1<V>>,
     {
         if IsVariant::is_variant_(&*this, vari) {
-            Some(VariantProxy::from_raw_mut(this))
+            Ok(VariantProxy::from_raw_mut(this))
         } else {
-            None
+            Err(this)
         }
     }
 
-    fn into_variant<V>(self, vari: FieldPath1<V>) -> Option<VariantProxy<Self, FieldPath1<V>>>
+    #[inline(always)]
+    fn into_variant<V>(
+        self,
+        vari: FieldPath1<V>,
+    ) -> Result<VariantProxy<Self, FieldPath1<V>>,Self>
     where
         Self: IsVariant<FieldPath1<V>> + Sized,
     {
         if IsVariant::is_variant_(&self, vari) {
-            unsafe { Some(VariantProxy::new(self)) }
+            unsafe { Ok(VariantProxy::new(self)) }
         } else {
-            None
+            Err(self)
         }
     }
 
     #[cfg(feature = "alloc")]
+    #[inline(always)]
     fn box_into_variant<V>(
         self: Box<Self>,
         vari: FieldPath1<V>,
-    ) -> Option<Box<VariantProxy<Self, FieldPath1<V>>>>
+    ) -> Result<Box<VariantProxy<Self, FieldPath1<V>>>,Box<Self>>
     where
-        Self: IsVariant<FieldPath1<V>> + Sized,
+        Self: IsVariant<FieldPath1<V>>,
     {
         if IsVariant::is_variant_(&*self, vari) {
-            unsafe { Some(VariantProxy::from_box(self)) }
+            unsafe { Ok(VariantProxy::from_box(self)) }
         } else {
-            None
+            Err(self)
         }
     }
 }
